c56d01c95c927b8d20d4359eace2b20bd6172fb4
abhinavps <149350440+abhi-123-art@users.noreply.github.com>
2023-11-23 11:38:29 +0530

    Add files via upload

---
+++ got.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+contentList=[]
+@app.route("/",methods=["GET"])
+def kingdom():
+    req=requests.get("https://thronesapi.com/api/v2/Characters")
+    content=req.json()
+    for data in content:
+        if data not in contentList:
+            contentList.append(data)
+    return render_template("index.html",output=contentList)
+
+if __name__=="__main__":
+    app.run(debug=True)
